170eb3da9bc21d4a3c4951f752539cce3d9c4726
Marc Wittke <marc@wittke-web.de>
2017-12-24 11:31:21 -0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    using System.Globalization;
+    using Properties;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string cultureString = "de"; // de-DE?
+            System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo(cultureString);
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new CultureInfo(cultureString);
+            Resources.Culture = new CultureInfo(cultureString);
+            
+            Console.WriteLine(Resources.Hello);
+
+            Console.WriteLine(Resources.ResourceManager.GetString("Hello", new CultureInfo(cultureString)));
+        }
+    }
+}
